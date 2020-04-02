f94295d922224fa87774a8ee6a0e4541e9b896aa
Rich Lynch <rlynch3456@yahoo.com>
2020-04-01 21:05:14 -0400

    Add program to switch colors when button is pushed

---
+++ neopixel_with_pushbutton_selector/neopixel_with_pushbutton_selector.ino
@@ -0,0 +1,110 @@
+/*
+ * neopixel_with_pushbutton_selector
+ * 
+ * This is a test program to do different effects
+ * with noepoixels selecting pushbutton
+ */
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN    6
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 8
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
+
+/* We will be using an interrupt to change colors */
+const byte interruptPin = 2;
+volatile byte state = HIGH;
+long debounce_time = 250;
+volatile unsigned long last_micros;
+int r, g, b;
+int lastState;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
+  // Any other board, you can remove this part (but no harm leaving it):
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  // END of Trinket-specific code.
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), interrupt, FALLING);
+  last_micros = micros();
+
+  randomSeed(analogRead(0));
+
+  Serial.begin(9600);
+
+  r = random(0, 255);
+  g = random(0, 255);
+  b = random(0, 255);
+
+  lastState = state;
+  setAll(r, g, b);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  /* Did the state change (did the button get pressed)? */
+  if(lastState != state) {
+    lastState = state;
+    setAll(r, g, b);
+  }
+}
+
+
+/* Do something everytime the interrupt is received */
+void interrupt(){
+
+  /* We have to take into account that the program has been running for 
+   *  a long time, so micros() could have looped back around to zero.  This
+   *  happens abut every 70 minutes.
+   */
+  if(((long)(micros() - last_micros) >= debounce_time * 1000) || (micros() < last_micros)){
+    state = !state;
+    last_micros = micros();
+    changeColor();
+  }
+}
+
+/* Change the global rgb colors */
+void changeColor() {
+  r = random(0, 255);
+  g = random(0, 255);
+  b = random(0, 255);  
+}
+
+
+/* Set all of the pixels to specified rgb color */
+void setAll(byte red, byte green, byte blue) {
+  for(int i = 0; i < LED_COUNT; i++ ) {
+    strip.setPixelColor(i, red, green, blue);
+  }
+  strip.show();
+}
